3e7fcf1cc570a056e56885eeeb6fe1b58239a7bc
Nino de la Cruz <nino.firstbostonsoftware@gmail.com>
2022-02-05 14:58:03 +0800

    display number of users in the dashboard and created payment and API page

--- app/Http/Controllers/Dashboard/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/Dashboard/DashboardController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers\Dashboard\Dashboard;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use DB;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard.dashboard.index');
+        $get_total_users= DB::table('users')->count();
+        $get_total_assets = DB::table('assets')->where('post_type', 'publish')->where('is_delete', 0)->count();
+        return view('dashboard.dashboard.index',compact('get_total_users', 'get_total_assets')) ;
     }
 }
