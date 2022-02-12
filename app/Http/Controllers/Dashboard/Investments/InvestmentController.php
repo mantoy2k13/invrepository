8fe01b342d71081141100a62376c461adb1be83c
Nino de la Cruz <nino.firstbostonsoftware@gmail.com>
2022-02-12 15:01:30 +0800

    Investment Functionalities and sum of investments

--- app/Http/Controllers/Dashboard/Investments/InvestmentController.php
+++ app/Http/Controllers/Dashboard/Investments/InvestmentController.php
@@ -5,36 +5,34 @@ namespace App\Http\Controllers\Dashboard\Investments;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Yajra\Datatables\Datatables;
-
-
 use App\Models\Investment;
-
+use App\Models\User;
 use Validator;
 use Redirect;
 use Auth;
-
 use DB;
 
 class InvestmentController extends Controller
 {
     // Admin
     public function investments(){
-        // $user = DB::table('users')
-        //     ->leftJoin('personal_information', 'users.id', '=', 'personal_information.user_id')
-        //     ->where('users.id', Auth::user()->id)
-        // ->first();
         return view('dashboard.investments.investment');
     }
     // Members
     public function myInvestments(){
-        return view('dashboard.investments.my-investment');
+        $get_total_investment=  DB::table('investments')->sum('invest_amount');
+        return view('dashboard.investments.my-investment',compact('get_total_investment'));
     }
 
     public function storeInvestment(Request $request){
-        $rules = [
-            'invest_amount'     => 'required',
-           
-        ];
+         $rules = [
+            'amount'            => 'required',
+            'card_number'       => 'required',
+            'card_name'         => 'required',
+            'card_expiry_month' => 'required',
+            'card_expiry_year'  => 'required',
+            'card_cvc'          => 'required'
+         ];
 
         $validator = Validator::make($request->all(), $rules);
         if($validator->fails()){
@@ -42,11 +40,20 @@ class InvestmentController extends Controller
             return response()->json([ 'code' => 200, 'status' => false, 'msg' => $errorString, 'data' => null], 200);
         }else{
             // Update Product Info
-            $data                    = new Investment;
-            $data->invest_amount      = $request->invest_amount;
-           
-           
+            $user_id             = Auth::user()->id;
+            $data                = new Investment;
+            $data->user_id       = $user_id;
+            $data->invest_amount = $request->amount;
+            $data->card_number   = $request->card_number;
+            $data->card_name     = $request->card_name;
+            $data->card_exp	     = $request->card_expiry_month .'/'.$request->card_expiry_year;
+            $data->card_cvc	     = $request->card_cvc;
             $data->save();
+
+            $user_data = User::find($user_id);
+            $user_data->role = 'member';
+            $user_data->save();
+
             return response()->json(['code' => 200, 'status' => true, 'msg' => 'Investment saved successfully', 'data' => $data], 200);
         }
     }
--- resources/views/dashboard/investments/my-investment.blade.php
+++ resources/views/dashboard/investments/my-investment.blade.php
@@ -25,14 +25,14 @@
                     <div class="col-lg-6 col-sm-6">
                         <div class="widget-panel widget-style-2 bg-white">
                             <i class="md md-attach-money text-primary"></i>
-                            <h2 class="m-0 text-dark counter font-600">0.00</h2>
+                            <h2 class="m-0 text-dark counter font-600">{{ $get_total_investment }}</h2>
                             <div class="text-muted m-t-5">Today's Invest</div>
                         </div>
                     </div>
                     <div class="col-lg-6 col-sm-6">
                         <div class="widget-panel widget-style-2 bg-white">
                             <i class="md md-attach-money text-pink"></i>
-                            <h2 class="m-0 text-dark counter font-600">0.00</h2>
+                            <h2 class="m-0 text-dark counter font-600">{{ $get_total_investment }}</h2>
                             <div class="text-muted m-t-5">Total Invested</div>
                         </div>
                     </div>
@@ -43,7 +43,7 @@
                         <div class="card-box">
                             <div class="pull-right">
                                 <div class="btn-group">
-                                    <a href="javascript:;" onclick="addCash()" class="btn btn-inverse waves-effect waves-light btn-sm"><i class="ti-plus"></i> Add Cash</a>
+                                    <a href="javascript:;" onclick="showInvestmentModal()" class="btn btn-inverse waves-effect waves-light btn-sm"><i class="ti-plus"></i> Add Cash</a>
                                 </div>
                                 <div class="btn-group">
                                     <input type="hidden" id="investments_view_url" value="">
@@ -76,49 +76,6 @@
         </div>
 
         {{-- Add Cash Modal --}}
-        <div class="modal fade" id="addCashModasdl" role="dialog">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal">&times;</button>
-                        <h4 class="m-t-0 header-title"><b><i class="ti-plus"></i> Investment History</b></h4>
-                    </div>
-                    <form action="javascript:;" method="POST">
-                        <div class="modal-body">
-                            <form class="form-horizontal" id="example-form">
-                                <div class="form-group">
-                                    <label for="">Enter Amount</label>
-                                    <div class="input-group m-t-10">
-                                        <span class="input-group-addon btn-inverse"><i class="fa fa-dollar"></i></span>
-                                        <input oninput="validateNumber(this);" type="text" id="amount" name="amount" class="form-control" placeholder="0.00">
-                                    </div>
-                                </div>
-                                <div class="card-js form-group">
-                                    <!-- Card number -->
-                                    <input class="card-number form-control" name="field__card-number" placeholder="Enter your card number" autocomplete="off" required>
-                                    <!-- Name on card -->
-                                    <input class="name form-control" id="the-card-name-id" name="field__card-name" placeholder="Enter the name on your card" autocomplete="off" required>
-                                    <!-- Card expiry (element that is displayed) -->
-                                    <input class="expiry form-control" autocomplete="off" required>
-                                    <!-- Card expiry - Month (hidden) -->
-                                    <input class="expiry-month" name="field__card-expiry-month">
-                                    <!-- Card expiry - Year (hidden) -->
-                                    <input class="expiry-year" name="field__card-expiry-year">
-                                    <!-- Card CVC -->
-                                    <input class="cvc form-control" name="field__card-cvc" autocomplete="off" required>
-                                </div><!-- END .card-js wrapper -->
-                            </form>
-                        </div>
-                        <div class="modal-footer">
-                            <a href="javascript:;" class="btn btn-inverse waves-effect waves-light btn-sm"><i class="ti-check"></i> Add Cash</a>
-                            <a href="javascript:;" class="btn btn-danger waves-effect waves-light btn-sm"><i class="ti-close"></i> Cancel</a>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-
-        <!-- sample modal content -->
         <div id="addCashModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
@@ -129,34 +86,36 @@
                     <div class="modal-body">
                         <div class="row">
                             <div class="col-md-12">
-                                <form class="form-horizontal" id="example-form">
+                                <form action="javascript:;" class="form-horizontal" id="investmentForm">
                                     <div class="form-group">
                                         <label for="">Enter Amount</label>
                                         <div class="input-group m-t-10">
                                             <span class="input-group-addon btn-inverse"><i class="fa fa-dollar"></i></span>
-                                            <input oninput="validateNumber(this);" type="text" id="amount" name="amount" class="form-control" placeholder="0.00">
+                                            <input oninput="validateNumber(this);" type="text" id="amount" name="amount" class="form-control" placeholder="0.00" required>
                                         </div>
                                     </div>
                                     <div class="card-js form-group">
                                         <!-- Card number -->
-                                        <input class="card-number form-control" name="field__card-number" placeholder="Enter your card number" autocomplete="off" required>
+                                        <input class="card-number form-control" name="card_number" placeholder="Enter your card number" autocomplete="off" required>
                                         <!-- Name on card -->
-                                        <input class="name form-control" id="the-card-name-id" name="field__card-name" placeholder="Enter the name on your card" autocomplete="off" required>
+                                        <input class="name form-control" id="the-card-name_id" name="card_name" placeholder="Enter the name on your card" autocomplete="off" required>
                                         <!-- Card expiry (element that is displayed) -->
                                         <input class="expiry form-control" autocomplete="off" required>
                                         <!-- Card expiry - Month (hidden) -->
-                                        <input class="expiry-month" name="field__card-expiry-month">
+                                        <input class="expiry-month" name="card_expiry_month">
                                         <!-- Card expiry - Year (hidden) -->
-                                        <input class="expiry-year" name="field__card-expiry-year">
+                                        <input class="expiry-year" name="card_expiry_year">
                                         <!-- Card CVC -->
-                                        <input class="cvc form-control" name="field__card-cvc" autocomplete="off" required>
+                                        <input class="cvc form-control" name="card_cvc" autocomplete="off" required>
                                     </div><!-- END .card-js wrapper -->
                                 </form>
                             </div>
                         </div>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-inverse waves-effect waves-light"><i class="ti-check"></i> Submit Payment</button>
+                        {{-- Default Values --}}
+                        <input type="hidden" id="investUrl" value="{{ route('investment.store') }}">
+                        <button type="button" onclick="submitCash(this, 'investmentForm')" class="btn btn-inverse waves-effect waves-light has-spinner"><i class="ti-check"></i> Submit Payment</button>
                         <button type="button" class="btn btn-danger waves-effect" data-dismiss="modal">Close</button>
                     </div>
                 </div><!-- /.modal-content -->
